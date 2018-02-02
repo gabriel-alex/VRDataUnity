b0c8e6d868dc75bb6c0b0a6f5e0f531498a8a678
4lexTristan <4lex(pt)tristan(at)gmail(pt)com>
2018-02-02 16:29:37 +0100

    joystick scene + waypoint scene
    untity project with 2 differents scene
    update of the collecting data script

---
+++ UnityProjectVRdata/Assets/Scripts/PositionDataCollect.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionDataCollect : MonoBehaviour {
+
+	private string[] positionData;
+	private string function_name = "PositionDataCollect";
+	[SerializeField] private bool debug = false;
+	[SerializeField] private string service_url;
+	[SerializeField] private bool random_user_id = false;
+	[SerializeField] private string user_ID;
+	public Camera cameraObj;
+
+	void Start(){
+		if (random_user_id == true) {
+			int randNumber = Random.Range (0, 100000);
+			user_ID = "User_" + randNumber.ToString("D6");
+		} 
+		if (debug) {
+			Debug.Log ("["+function_name+"]user id: "+user_ID);
+		}
+	}
+
+	void Update(){
+		float[] position = {transform.position.x, transform.position.y, transform.position.z};
+		float[] rotation = {cameraObj.transform.position.x, cameraObj.transform.position.y, cameraObj.transform.position.z};
+		postData( position, rotation);
+	}
+
+	private void postData(float[] pos, float[] rot){
+		WWWForm form = new WWWForm();
+
+		form.AddField ("user_id", user_ID);
+		form.AddField ("px", pos[0].ToString("G"));
+		form.AddField ("py", pos[1].ToString("G"));
+		form.AddField ("pz", pos[2].ToString("G"));
+		form.AddField ("rx", rot[0].ToString("G"));
+		form.AddField ("ry", rot[1].ToString("G"));
+		form.AddField ("rz", rot[2].ToString("G"));
+		if (debug) {
+			Debug.Log ("["+function_name+"] X:"+pos[0].ToString("G")+" Y:"+pos[1].ToString("G")+" Z:"+pos[2].ToString("G"));
+		}
+		WWW www = new WWW (service_url, form);
+		/*yield return www;
+		if (!string.IsNullOrEmpty (www.error)) {
+			Debug.Log ("["+function_name+"]" + www.error);
+		} else if (debug) {
+			Debug.Log ("["+function_name+"]Send accomplished");
+		}*/
+	}
+
+}
---
+++ UnityProjectVRdata/Assets/Scripts/VR_FPC.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VR_FPC : MonoBehaviour {
+
+	private Vector2 m_Input;
+	private CharacterController m_CharacterController;
+	[SerializeField] private float speed;
+	[SerializeField] private float m_GravityMultiplier;
+	private Vector3 m_MoveDir = Vector3.zero;
+	public bool debug = false;
+	private Camera m_cam;
+
+	// Use this for initialization
+	void Start () {
+		m_CharacterController = GetComponent<CharacterController> ();
+		m_cam = Camera.main;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		GetInput ();
+
+		Vector3 desiredMove = m_cam.transform.forward * m_Input.y + m_cam.transform.right * m_Input.x;
+
+		/*RaycastHit hitInfo;
+		Physics.SphereCast (transform.position, m_CharacterController.radius, Vector3.down, out hitInfo, m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+		desiredMove = Vector3.ProjectOnPlane (desiredMove, hitInfo.normal).normalized;*/
+
+		m_MoveDir.x = desiredMove.x * speed;
+		m_MoveDir.z = desiredMove.z * speed;
+
+		m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
+		m_CharacterController.Move (m_MoveDir * Time.fixedDeltaTime);
+
+
+	}
+	private void GetInput(){
+		float horizontal = Input.GetAxis("Horizontal");
+		float vertical = Input.GetAxis("Vertical");
+
+		m_Input = new Vector2 (horizontal, vertical);
+		/*if (m_Input.sqrMagnitude > 1) {
+			m_Input.Normalize ();
+		}*/
+		if (debug) {
+			Debug.Log ("horizontal input value" + m_Input.x + ": Vertical input value" + m_Input.y);
+		}
+	}
+}
